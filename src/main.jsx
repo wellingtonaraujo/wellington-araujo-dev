6ad9c5f6a5591de74247050b253ec3041d2cffbf
wellington de araujo ferreira <wellington.araujoferreira@gmail.com>
2026-05-13 09:14:18 -0300

    refactor: ajusta inicialização da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import './index.css'
-
-// function App() {
-//   return (
-//     <div className="min-h-screen bg-zinc-950 text-white">
-//       <h1 className="text-5xl font-black text-center pt-20">
-//         Wellington de Araújo Ferreira
-//       </h1>
-//     </div>
-//   )
-// }
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
